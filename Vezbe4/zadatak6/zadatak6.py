0702399b06dc5c197c70e6e20c21b5a6e33a5109
aleksalazic <56800979+aleksalazic@users.noreply.github.com>
2019-10-22 18:31:33 +0200

    Add files via upload

---
+++ Vezbe4/zadatak6/zadatak6.py
@@ -0,0 +1,46 @@
+import re
+
+def main():
+    neformatiran = open('neformatiranTekst.txt', 'r')
+    formatiran = open('formatiran.txt', 'a')
+
+    naslov = neformatiran.readline()
+    naslov = re.sub(' +', ' ', naslov).strip().title().center(100)
+    
+    print('{}\n'.format(naslov), file=formatiran)
+
+    
+    #Ovo dole verovatno moze more pythonic da se uradi, 
+    #ali nije mi python primaran jezik pa ne zamerite
+
+
+    for paragraf in neformatiran:
+        recenice_sa_tackama = []
+        recenice_sa_upitnikom = []
+        paragraf = re.sub(' +', ' ', paragraf)
+        
+        
+        for recenica_sa_tackom in paragraf.split('.'):
+            recenica_sa_tackom = recenica_sa_tackom.strip().capitalize()
+            if recenica_sa_tackom.find('?') != -1:
+                
+                for recenica_sa_upitnikom in recenica_sa_tackom.split('?'):
+                    recenica_sa_upitnikom = recenica_sa_upitnikom.strip().capitalize()
+                    recenice_sa_upitnikom.append(recenica_sa_upitnikom)
+
+                recenica_sa_tackom = '? '.join(recenice_sa_upitnikom)
+
+            recenice_sa_tackama.append(recenica_sa_tackom)
+        
+        paragraf = '. '.join(recenice_sa_tackama)
+        print(5*' ' + paragraf, file=formatiran)
+
+
+    neformatiran.close()
+    formatiran.close()
+
+
+
+
+
+main()
